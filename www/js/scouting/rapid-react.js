16e3ae492993bc81522ff4e9f8b75f2b1a575675
kevin129-code <78120570+kevin129-code@users.noreply.github.com>
2022-02-07 18:49:50 -0800

    Finished button making

--- www/js/scouting/rapid-react.js
+++ www/js/scouting/rapid-react.js
@@ -4,32 +4,29 @@ let TeleD = document.getElementById("TeleOp")
 let EndGAmeD = document.getElementById("end")
 let submitD = document.getElementById("submit")
 let TarmacD = document.getElementById("AutoLeftTarmacCheckBox")
-let UpperHubnum = document.getElementById("UpperHubNum")
 var UpperHubNumber = 0;
 //UpperHubnum.innerText = UpperHubNumber;
 //console.log(TarmacD.checked);
 
-function btnAdd(UpperHubNum){ 
-
-UpperHubNumber == UpperHubNumber + 1;
-UpperHubnum.innerText.replace("0", "testing?");
-
+function btnAdd(buttonId){ 
+    document.getElementById(buttonId).innerHTML++;
 }
 
-function btnSub(UpperHubNum){ 
-    let label = document.getElementById(UpperHubNum);
-    UpperHubNumber++;
-    UpperHubnum.innerText.replaceAll(UpperHubNumber);
-    
-    
-    }
+function btnSub(buttonId){ 
+    document.getElementById(buttonId).innerHTML--;
+}
 
-function AutoCall(){
-
+function resetTabs(){
     submitD.classList.replace("visible", "invisible");
     autoD.classList.replace("invisible", "visible");
     EndGAmeD.classList.replace("visible", "invisible");
-   TeleD.classList.replace("visible", "invisible");
+   TeleD.classList.replace("visible", "invisible");6
+}
+
+function switchTab(tabId){
+    let selectedTab = document.getElementById(tabId);
+    selectedTab.classList = "sm:px-12 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-800 text-gray-800 tracking-wider rounded-t";
+    
 
 }
 
