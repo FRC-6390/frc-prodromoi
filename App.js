d3c2cff375d6328f3f0a1cd5d756e6064bd50dd7
kevin129-code <78120570+kevin129-code@users.noreply.github.com>
2023-02-20 21:26:46 -0800

    Still working on Screen naviagtion between Files

--- App.js
+++ App.js
@@ -1,63 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, Button, Alert,  View, Pressable } from 'react-native';
-import mainScreen from './js/MainScreen';
+import mainScreen from './Screens/Matches/MainScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Button1 from './Typescript/Buttons';
-import FlexDirectionBasics from './js/MainScreen';
+//import {Button1, navigatorScreen} from './Screens/Matches/Buttons';
+import {FlexDirectionBasics} from './Screens/Matches/MainScreen';
+import { Button1, navigatorScreen } from './Screens/Matches/Buttons';
+
 
 const Stack = createNativeStackNavigator();
  
-
-
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'center', margin: '10', padding: '20'}}>
+    <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '10', padding: '20'}}>
      
-        <Pressable style={styles.button}   onPress={() => navigation.navigate('Details')}>
-        <Text style={styles.text}>{"Details"}</Text>
+        <Pressable style={styles.button} onPress={() => navigation.navigate('Team2')}>
+        <Text style={styles.text}>{"Matches"}</Text>
       </Pressable>
   
-  
-       <Pressable style={styles.button} onPress={() => navigation.navigate('Team')}>
-        <Text style={styles.text}>{"Go to Teams"}</Text>
+      <Pressable style={styles.button} onPress={() => navigation.navigate('Team')}>
+        <Text style={styles.text}>{"Results"}</Text>
         </Pressable>
+
       
     </View>
   );
 }
 
-function DetailsScreen() {
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    
-    </View>
-  );
-  
-}
-
-function Teams(){
+//Testing Function
+function Teams({navigation}){
 return(
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
   <Text>Teams Screen</Text>
+
   
+       <Pressable style={styles.button} onPress={() => navigation.push('Team')}>
+        <Text style={styles.text}>{"go to Teams2"}</Text>
+        </Pressable>
+
 </View>
-
 );
-
 }
-
+//*************************************************************************************************** */
 
 export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Details" component={Button1} />
+    <Stack.Screen name="Team2" component={navigatorScreen}/>
       <Stack.Screen name="Team" component={FlexDirectionBasics} />
+      <Stack.Screen name="Details"component={Button1}/>
     </Stack.Navigator>
   </NavigationContainer>
 
@@ -79,15 +74,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
-    paddingVertical: 10,
-    paddingHorizontal: 10,
+    paddingVertical: 30,
+    paddingHorizontal: 50,
     margin: 4,
     borderRadius: 4,
     elevation: 3,
     backgroundColor: 'red',
   },
   text: {
-    fontSize: 16,
+    fontSize: 22,
     lineHeight: 21,
     fontWeight: 'bold',
     letterSpacing: 1.5,
--- Screens/Matches/MainScreen.js
+++ Screens/Matches/MainScreen.js
@@ -91,4 +91,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default FlexDirectionBasics;
+export {FlexDirectionBasics};
